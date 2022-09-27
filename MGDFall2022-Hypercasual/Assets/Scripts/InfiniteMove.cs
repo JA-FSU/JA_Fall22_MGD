99f4252ef7cf50eae5ad27b77a623f9b7f39a883
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2022-09-27 01:47:04 -0400

    Got infinite scrolling to work

--- MGDFall2022-Hypercasual/Assets/Scripts/InfiniteMove.cs
+++ MGDFall2022-Hypercasual/Assets/Scripts/InfiniteMove.cs
@@ -6,19 +6,19 @@ public class InfiniteMove : MonoBehaviour
 {
     private Vector3 startPos;
     private float repeatWidth;
-    private float divide = 0.5f;
+    private float divide = 3.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider>().size.z / 2;
+        repeatWidth = transform.localScale.z / divide;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > startPos.z - repeatWidth)
+        if (transform.position.z < startPos.z - repeatWidth)
         {
             transform.position = startPos;
         }
