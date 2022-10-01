d2283ccd0d1e0d54911a1c42ba3c042c3a20f112
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2022-10-01 00:36:36 -0400

    Added sound effects

--- MGDFall2022-Hypercasual/Assets/Scripts/PlayerController.cs
+++ MGDFall2022-Hypercasual/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private GameManager gameManager;
+    private AudioSource audioSource;
+    public AudioClip[] SE;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -41,12 +44,14 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Destroy(other.gameObject);
+            audioSource.PlayOneShot(SE[0]);
         }
 
         if (other.gameObject.CompareTag("Food") || other.gameObject.CompareTag("Pancakes"))
         {
             Destroy(other.gameObject);
             gameManager.UpdateScore(10.0f);
+            audioSource.PlayOneShot(SE[1]);
         }
 
         if (other.gameObject.CompareTag("Food"))
